7e612e2a306427bf6f2aacfc269263f00b4b244a
Dylan Dimayuga <dylandimayuga@yahoo.com>
2021-09-29 21:15:09 -0500

    FINISH THE PROJECT AFTER SO MUCH TRIAL AND ERROR! I FORGOT TO COMMIT BECAUSE I WAS TOO FOCUSED ON FINISHING!

--- script.js
+++ script.js
@@ -10,21 +10,52 @@ var generateBtn = document.querySelector("#generate");
 // }
 
 function generatePassword() {
-  var stringToReturn = "ahsdfasd"
-  var lengthAnswer = prompt(passLength); // this happens in an instant and is lost--never stored in vaariable
+  var stringToReturn = ""
+  var lengthAnswer = prompt(passLength); // this happens in an instant and is lost--never stored in variable
   if(lengthAnswer >= 8 && lengthAnswer <=128) {
     var lowerAnswer = confirm(lowerChar);
   } else{
-    alert("Please enter a valid length")
+    alert("Please enter a valid length") & generatePassword();
   }
-  
+
   var upperAnswer = confirm(upperChar); 
   var numAnswer = confirm(numbers);
   var specialAnswer = confirm(specialChar);
 
+  var abc = "abcdefghijklmnopqrstuvwxyz";
+  var num = "0123456789";
+  var upperabc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var special = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+  
+  var charString = "";
+
+  if (lowerAnswer === true){
+    charString += abc;
+  }
+  if (upperAnswer === true){
+    charString += upperabc;
+  }
+  if (numAnswer === true){
+    charString += num;
+  }
+  if (specialAnswer === true){
+    charString += special;
+  }
+
+  if (lowerAnswer === false && upperAnswer === false && numAnswer === false && specialAnswer === false) {
+    alert("Needs to include at least one type of character for the password.") + generatePassword();
+  }
+
+  for (i = 1; i <= lengthAnswer; i++) {
+    stringToReturn += charString.charAt(Math.floor(Math.random() * charString.length));
+    console.log(stringToReturn + "Inside while Loop");
+  }
+  console.log(stringToReturn + "Outside while loop");
+  console.log(charString)
 
   // return a string that fits the criteria based on the answers given.
   return stringToReturn;
+
 }
 
 // Write password to the #password input
